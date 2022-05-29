f3e62030373a8f3d487518fde6874639080e52f0
JavierS084 <javier.sanchezb084@gmail.com>
2022-05-28 22:02:30 -0400

    V1.0

---
+++ ZK/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace ZK
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblHuellas = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblAsistencias = new System.Windows.Forms.Label();
+            this.Desabilitar = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(27, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(937, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(689, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(242, 28);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(27, 175);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(474, 319);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(538, 175);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(467, 319);
+            this.listBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FloralWhite;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(27, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Marcajes de Asistencia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(618, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Usuarios";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FloralWhite;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(538, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Marcajes Operativos";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FloralWhite;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(225, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Total de Huellas Registradas ";
+            // 
+            // lblHuellas
+            // 
+            this.lblHuellas.AutoSize = true;
+            this.lblHuellas.BackColor = System.Drawing.Color.FloralWhite;
+            this.lblHuellas.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblHuellas.Location = new System.Drawing.Point(400, 17);
+            this.lblHuellas.Name = "lblHuellas";
+            this.lblHuellas.Size = new System.Drawing.Size(15, 17);
+            this.lblHuellas.TabIndex = 9;
+            this.lblHuellas.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FloralWhite;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(454, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(198, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total de Marcajes de Asistencias";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblAsistencias
+            // 
+            this.lblAsistencias.AutoSize = true;
+            this.lblAsistencias.BackColor = System.Drawing.Color.FloralWhite;
+            this.lblAsistencias.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAsistencias.Location = new System.Drawing.Point(658, 17);
+            this.lblAsistencias.Name = "lblAsistencias";
+            this.lblAsistencias.Size = new System.Drawing.Size(15, 17);
+            this.lblAsistencias.TabIndex = 11;
+            this.lblAsistencias.Text = "0";
+            // 
+            // Desabilitar
+            // 
+            this.Desabilitar.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Desabilitar.Location = new System.Drawing.Point(124, 12);
+            this.Desabilitar.Name = "Desabilitar";
+            this.Desabilitar.Size = new System.Drawing.Size(86, 27);
+            this.Desabilitar.TabIndex = 12;
+            this.Desabilitar.Text = "Desabilitar";
+            this.Desabilitar.UseVisualStyleBackColor = true;
+            this.Desabilitar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(273, 142);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(228, 27);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Obtener Marcajes de Asistencias";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(799, 142);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(206, 27);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Obtener Marcajes Operativos";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1042, 593);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.Desabilitar);
+            this.Controls.Add(this.lblAsistencias);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblHuellas);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private ComboBox comboBox1;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label lblHuellas;
+        private Label label5;
+        private Label lblAsistencias;
+        private Button Desabilitar;
+        private Button button4;
+        private Button button5;
+    }
+}
---
+++ ZK/Form1.cs
@@ -0,0 +1,86 @@
+using ZKSoftwareAPI;
+namespace ZK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ZKSoftware dispositivo = new ZKSoftware(Modelo.X628C);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Desconectar")
+            {
+                button1.Text = "Conectar";
+                dispositivo.DispositivoDesconectar();
+                return;
+
+            }
+            if (!dispositivo.DispositivoConectar("192.168.17.16", 3, true))
+            {
+                MessageBox.Show(dispositivo.ERROR);
+            }
+            else
+            {
+                button1.Text = "Desconectar";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //buscar usuarios del dispositivos
+            if (dispositivo.UsuarioBuscarTodos(true))
+            {
+                comboBox1.DataSource = dispositivo.ListaUsuarios;
+            }
+            else
+            {
+                MessageBox.Show(dispositivo.ERROR);
+
+            }
+            //consultar total de huellas registradas
+            dispositivo.DispositivoConsultar(NumeroDe.HuellasRegistradas);
+            lblHuellas.Text = dispositivo.ResultadoConsulta.ToString();
+
+            //consultar total de marcajes de asistencias
+            dispositivo.DispositivoConsultar(NumeroDe.RegistrosDeAsistencias);
+            lblAsistencias.Text = dispositivo.ResultadoConsulta.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Desabilitar.Text == "Desabilitar")
+            {
+                Desabilitar.Text = "Habilitar";
+                dispositivo.DispositivoCambiarEstatus(Estatus.Deshabilitar);
+            }
+            else
+            {
+                Desabilitar.Text = "Habilitar";
+                dispositivo.DispositivoCambiarEstatus(Estatus.Habilitar);
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dispositivo.DispositivoObtenerRegistrosAsistencias();
+            listBox1.DataSource = dispositivo.ListaMarcajes;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dispositivo.DispositivoObtenerRegistrosOperativos();
+            listBox2.DataSource = dispositivo.ListaMarcajes;
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
